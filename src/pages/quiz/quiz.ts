fc235dab9ad3f0ec695286b8686c869ee1dc2a74
werner.holzapfel <werner.holzapfel@trivento.nl>
2017-03-26 11:40:46 +0200

    use observable

--- src/pages/quiz/quiz.ts
+++ src/pages/quiz/quiz.ts
@@ -1,23 +1,27 @@
-import {Component, ViewChild} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from "@angular/core";
+import {NavController} from "ionic-angular";
 import {Data} from "../../providers/data";
+import {TimerObservable} from "rxjs/observable/TimerObservable";
+import {Subscription} from "rxjs";
+
 
 /*
-  Generated class for the Quiz page.
+ Generated class for the Quiz page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   selector: 'page-quiz',
   templateUrl: 'quiz.html'
 })
 export class Quizpage {
   @ViewChild('slides') slides: any;
-
-  hasAnswered: boolean = false;
+  countdown: any;
+  timer: Subscription;
   score: number = 0;
-
+  aantalVragen: number;
+  actieveVraag: number = 1;
   slideOptions: any;
   questions: any;
 
@@ -26,47 +30,52 @@ export class Quizpage {
     this.slideOptions = {
       onlyExternal: true
     };
-
   }
 
   ionViewDidLoad() {
 
     this.dataService.load().then((data) => {
-
+      this.aantalVragen = data.length;
       data.map((question) => {
-
         let originalOrder = question.answers;
         question.answers = this.randomizeAnswers(originalOrder);
         return question;
-
       });
-
       this.questions = data;
-
     });
-
   }
 
-  nextSlide(){
+  nextSlide(actieveVraag) {
     this.slides.slideNext();
+    if (this.actieveVraag <= this.aantalVragen) {
+      this.countdown = 6;
+      this.timer = TimerObservable
+        .interval(1000 /* ms */)
+        .timeInterval()
+        .take(this.countdown)
+        .subscribe(() => {
+          this.countdown--;
+          console.log(this.timer);
+        }, () => {
+          // error
+        }, () => {
+          console.log("next slide");
+          this.timer.unsubscribe();
+          this.nextSlide(this.actieveVraag++);
+        });
+    }
   }
 
-  selectAnswer(answer, question){
-
-    this.hasAnswered = true;
-    answer.selected = true;
-    question.flashCardFlipped = true;
-
-    if(answer.correct){
+  selectAnswer(answer, question) {
+    if (answer.correct) {
       this.score++;
     }
 
-    setTimeout(() => {
-      this.hasAnswered = false;
-      this.nextSlide();
-      answer.selected = false;
-      question.flashCardFlipped = false;
-    }, 3000);
+    this.countdown = 0;
+    this.timer.unsubscribe();
+    this.countdown = 0;
+
+    this.nextSlide(this.actieveVraag++);
   }
 
   randomizeAnswers(rawAnswers: any[]): any[] {
@@ -82,9 +91,24 @@ export class Quizpage {
 
   }
 
-  restartQuiz(){
+  //restart on
+  restartQuiz() {
+    this.actieveVraag = 1;
     this.score = 0;
     this.slides.slideTo(1, 1000);
+    this.timer = TimerObservable
+      .interval(1000 /* ms */)
+      .timeInterval()
+      .take(this.countdown)
+      .subscribe(() => {
+        this.countdown--;
+        console.log(this.timer);
+      }, () => {
+        // error
+      }, () => {
+        console.log("next slide");
+        this.timer.unsubscribe();
+        this.nextSlide(this.actieveVraag);
+      });
   }
-
 }
