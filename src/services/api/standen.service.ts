82578d340d15fc5f5bf8cabaf650409f7cbc9c99
wholzapfel <werner.holzapfel@trivento.nl>
2017-11-15 21:15:44 +0100

    totaalstand

--- src/services/api/standen.service.ts
+++ src/services/api/standen.service.ts
@@ -7,13 +7,13 @@ import {totaalstandModel} from "../../models/totaalstand";
 
 @Injectable()
 export class StandenService {
-  api = 'https://mollotenapi.herokuapp.com/api';
+  api = 'https://molapi.herokuapp.com/api/v1';
 
   constructor(public http: Http, private authHttp: AuthHttp) {
   }
 
   gettotaalstand(): Observable<totaalstandModel[]> {
-    return this.http.get(`${this.api}/totaalstand`)
+    return this.http.get(`${this.api}/standen`)
       .map(res => <totaalstandModel[]>res.json());
   }
 
